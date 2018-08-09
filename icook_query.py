79066b291f60e7253160eb6943bc926a0d1a09e0
Roger Lo <rogerlo@emotibot.com.cn>
2018-08-09 10:32:57 +0800

    change interval, remove duplicated

--- icook_query.py
+++ icook_query.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 
 max_page = 100
 items = []
+url_map = set()
 def query(query, ingredients=""):
     for i in tqdm(range(max_page)):
         try:
@@ -29,10 +30,12 @@ def query(query, ingredients=""):
                     "href": "https://icook.tw" + a["href"],
                     "favorite_count": int(favorite_count.strip().replace(",", ""))
                 }
-                items.append(item)
+                if item["href"] not in url_map:
+                    items.append(item)
+                    url_map.add(item["href"])
         except:
             print(recipe_card)
             traceback.print_exc()
             break
-        time.sleep(random.randint(1,25)/5)
+        time.sleep(random.randint(1,15)/5)
     return sorted(items, key=lambda x:x["favorite_count"], reverse=True)
